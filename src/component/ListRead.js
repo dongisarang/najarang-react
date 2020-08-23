0c3b8be1868dec2d17d15831e23f7eaea1d271b9
momomingzhi <momomingzhi@gmail.com>
2020-08-23 22:54:17 +0900

    [feat] read content 추가

--- src/component/ListRead.js
+++ src/component/ListRead.js
@@ -7,6 +7,7 @@ import { FaRegThumbsUp } from "react-icons/fa";
 import { BsChatDots } from "react-icons/bs";
 import { BsBookmark } from "react-icons/bs";
 import { observer, inject } from "mobx-react";
+import {AiOutlineClockCircle} from "react-icons/ai";;
 @inject("topic")
 @inject("currentTopic")
 class ListRead extends Component{
@@ -15,11 +16,34 @@ class ListRead extends Component{
     };
     render(){
         const name = this.props.currentTopic.getCurrentTopic();
+        const title = '안녕하세요';
+        const userName = '티맥스스페이스';
+        const time = '2시간';
+        const view = '83';
+        const replyCnt = '3';
+        const contents='퇴근하고 싶다';
         return(
             <PageLayout>
             <CategoryLayout>
                 <span>토픽 {name}</span>
             </CategoryLayout>
+            <TitleLayout>
+                <h1>{title}</h1>
+            </TitleLayout>
+            <UserLayout>
+                <span>{userName}</span>
+            </UserLayout>
+            <TimeLayout>
+                <AiOutlineClockCircle></AiOutlineClockCircle>
+                <span>{time}</span>
+                <FaEye className='eye'></FaEye>
+                <span>{view}</span>
+                <BsChatDots className='reply'></BsChatDots>
+                <span>{replyCnt}</span>
+            </TimeLayout>
+            <ContentsLayout>
+                <span>{contents}</span>
+            </ContentsLayout>
         </PageLayout>
         )
     }
@@ -36,4 +60,45 @@ const CategoryLayout = styled.div`
 
     }
 `
+const TitleLayout = styled.div`
+    display:flex;
+    flex-direction:row;
+    margin:20px 0px 1px 20px;
+`;
+const UserLayout = styled.div`
+    display:flex;
+    flex-direction:row;
+    margin:0px 0px 10px 20px;
+    span{
+        font-size:1px;
+    }
+`;
+const TimeLayout = styled.div`
+    display:flex;
+    flex-direction:row;
+    margin:0px 0px 0px 20px;
+    padding:0px 0px 20px 0px; 
+    border-bottom:1px solid;
+    span{
+        font-size:1px;
+        margin:0px 0px 0px 5px;
+    }
+    .eye{
+        margin:0px 0px 0px 15px;
+    }
+    .reply{
+        margin:0px 0px 0px 15px;
+    }
+`;
+const ContentsLayout = styled.div`
+    display:flex;
+    flex-direction:column;
+    margin:0px 0px 10px 20px;
+    height:100px;
+    border-bottom:1px solid;
+    span{
+        margin:20px 0px 10px 20px;
+        font-size:10px;
+    }
+`;
 export default ListRead;
